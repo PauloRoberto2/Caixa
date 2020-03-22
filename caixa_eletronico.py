75580caea6c3693ef4c43bde095318015f18cf2b
Paulão <paulo22game22@gmail.com>
2020-03-22 14:02:21 -0300

    Adição de celulas

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,10 +1,10 @@
 titulo = "PAULÃO'S BANK"
-print('=' *30)
+print('=' * 30)
 print(titulo.center(30))
-print('=' *30)
+print('=' * 30)
 valor = int(input('Que valor você quer sacar? R$'))
 total = valor
-céd = 50 
+céd = 100
 totcéd = 0
 while True:
     if total >= céd:
@@ -13,14 +13,20 @@ while True:
     else:
         if totcéd > 0:
             print(f'Total de {totcéd} cédulas de R${céd}')
-        if céd == 50:
+        if céd == 100:
+            céd = 50
+        elif céd == 50:
             céd = 20
         elif céd == 20:
             céd = 10
         elif céd == 10:
-            céd = 1
+            céd = 5
+        elif céd == 5:
+            céd = 2
         totcéd = 0
         if total == 0:
             break
-print('=' * 30)
-print("Volte sempre ao PAULÃO'S BANK! Tenha um bom dia!")
+final = "Volte sempre ao PAULÃO'S BANK! Tenha um bom dia!"
+print('=' * 50)
+print(final.center(50))
+print('=' * 50)
